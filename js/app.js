2eae0e82e737f58d505c07c5884178d581f288dd
Nazar-bnm <nazar.bnm@gmail.com>
2016-01-19 16:18:38 +0200

    Fixed app.js

--- js/app.js
+++ js/app.js
@@ -2,37 +2,45 @@
     'use strict';
 
     function addButton() {
-        var button = document.createElement("button");
-        var div = document.getElementById('app');
+        var conteiner = document.getElementById('app');
+        var button = document.createElement('button');
+        conteiner.appendChild(button);
         button.textContent = "Create Table";
-        div.appendChild(button);
         button.onclick = function() {
-            var table = document.createElement("table");
-            div.appendChild(table);
-            table.style.border = "1px solid black";
-            table.style.width = "100%";
+            //  create table
+            var table = document.createElement('table');
+            var row;
+            var col;
+            var i;
+            var j;
             var num = 1;
-            for (var i = 0; i < 10; i++) {
-                var tr = document.createElement("tr");
-                table.appendChild(tr);
-                for (var j = 0; j < 10; j++) {
-                    var th = document.createElement("th");
-                    tr.appendChild(th);
-                    th.textContent = num++;
-                    th.style.border = "1px solid black";
+            conteiner.appendChild(table);
+            table.style.border = "1px solid black";
+            table.style.borderCollapse = "collapse";
+            table.style.width = "50%";
+            table.style.marginLeft = "300px";
+            for (i = 0; i < 10; i++) {
+                row = document.createElement('tr');
+                table.appendChild(row);
+                for (j = 0; j < 10; j++) {
+                    col = document.createElement('th');
+                    row.appendChild(col);
+                    col.textContent = num++;
+                    col.style.border = "1px solid black";
                 }
             }
-            var moveLeft = document.createElement("button");
-            var moveRight = document.createElement("button");
-            div.appendChild(moveRight);
-            div.appendChild(moveLeft);
-            moveRight.textContent = "move right";
-            moveLeft.textContent = "move left";
-            moveRight.onclick = function() {
-                table.style.marginLeft = "100px";
+            // create buttons
+            var leftButton = document.createElement('button');
+            var rightButton = document.createElement('button');
+            conteiner.appendChild(leftButton);
+            conteiner.appendChild(rightButton);
+            leftButton.textContent = "Move Left";
+            rightButton.textContent = "Move Right";
+            leftButton.onclick = function() {
+                table.style.marginLeft = parseFloat(table.style.marginLeft) - 100 + 'px';
             };
-            moveLeft.onclick = function() {
-                table.style.marginLeft = "0";
+            rightButton.onclick = function() {
+                table.style.marginLeft = parseFloat(table.style.marginLeft) + 100 + 'px';
             };
         };
     }
